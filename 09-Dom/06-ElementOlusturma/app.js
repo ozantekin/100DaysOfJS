f1093be7b4716af09be8c91dda1b97ca8afe5601
ozantekin <ardilozantekin@gmail.com>
2022-01-28 01:12:47 +0300

    element oluşturma eklendi

---
+++ 09-Dom/06-ElementOlusturma/app.js
@@ -0,0 +1,30 @@
+// element oluşturma
+const li=document.createElement("li");
+
+// class ekleme
+li.className="list-group-item list-group-item-warning";
+
+// attribute ekleme
+li.setAttribute("title","ozantekindev");
+li.setAttribute("data","5");
+
+// text node
+const text=document.createTextNode("ozantekindev"); // texti li ile ilişkilendirmemiz gerekiyor
+li.appendChild(text).;
+
+
+// çarpı icon ekleyelim
+const a=document.createElement("a");
+a.setAttribute("href","#");
+a.className="delete-item float-right";
+a.innerHTML='<i class="fas fa-times"></i>'; 
+// class ile karışmaması için "" bunun yerine '' bunu kullanman gerekli
+
+
+// a -> li ilave edelim
+li.appendChild(a);
+
+// li -> ul  ilave edelim
+document.querySelector("#task-list").appendChild(li);
+
+console.log(li);
