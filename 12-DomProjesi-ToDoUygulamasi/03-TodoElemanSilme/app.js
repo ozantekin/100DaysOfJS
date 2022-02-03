40c30562e21c36a4ec3c5d27347e75bcf0371dea
ozantekin <ardilozantekin@gmail.com>
2022-02-03 23:20:17 +0300

    Todo eleman silme tamamlandı

--- 12-DomProjesi-ToDoUygulamasi/03-TodoElemanSilme/app.js
+++ 12-DomProjesi-ToDoUygulamasi/03-TodoElemanSilme/app.js
@@ -6,9 +6,18 @@ const taskList= document.querySelector("#task-list");
 eventListeners();
 
 function eventListeners (){
+
+    // submit event
 form.addEventListener("submit",addNewItem);
+
+    // delete an item
+taskList.addEventListener("click",deleteItem);
+
+// delete all items
+btnDeleteAll.addEventListener("click",deleteAllItems);
 }
 
+// add new item
 function addNewItem(e){
 
     if(input.value=== ""){
@@ -36,3 +45,49 @@ function addNewItem(e){
     console.log(li);
     e.preventDefault
 }
+
+
+// delete an item
+function deleteItem(e){
+    
+    if(confirm("Are u sure?")){  // işlemi doğrulama alerti oluşturur 
+
+        if(e.target.className ==='fas fa-times'){
+            e.target.parentElement.parentElement.remove();
+        }
+
+    }
+
+    e.preventDefault(); // a elementi olduğu için bu refersh olmayı önler button gibi elementlerde kullanmaya gerek yoktur
+}
+
+// delete all items
+
+
+
+function deleteAllItems(){
+
+    if(confirm("are u sure ?")){ 
+
+    //taskList.innerHTML(""); // bütün lileri siler 2.yol ise aşağıda 
+
+ taskList.childNodes.forEach(function(item){ // çağıracağımız bir işlem olmadığı için function içine bir şey yazmamıza gerek yok içinde yapıp bitirecez
+
+    console.log(item); // bütün elemanları yazar text node DAHİL
+
+    if (item.nodeType===1){ // text nodelar karşımıza gelmez
+        item.remove();
+    }
+
+    });
+
+    }
+
+    e.preventDefault();
+}
+
+
+// reminder :
+// node list ile HTML collection arasındaki fark nodelist text nodeları da getirir
+
+ 
