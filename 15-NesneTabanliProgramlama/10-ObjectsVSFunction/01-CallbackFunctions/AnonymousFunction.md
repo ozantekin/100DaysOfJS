caa9ed01140c803e0ab28b84532e9074705cbaec
ozantekin <ardilozantekin@gmail.com>
2022-02-26 02:19:50 +0300

    CallbackFunctions eklendi

---
+++ 15-NesneTabanliProgramlama/10-ObjectsVSFunction/01-CallbackFunctions/AnonymousFunction.md
@@ -0,0 +1,3 @@
+<h1 align="center">Anonymous Function</h1>
+
+- Bilgisayar programlamada, anonim bir işlev, bir tanımlayıcıya bağlı olmayan bir işlev tanımıdır. Anonim işlevler genellikle daha yüksek düzeyde işlevlere iletilen veya bir işlev döndürmesi gereken daha yüksek düzeyde bir işlevin sonucunu oluşturmak için kullanılan argümanlardır.
---
+++ 15-NesneTabanliProgramlama/10-ObjectsVSFunction/01-CallbackFunctions/app.js
@@ -0,0 +1,101 @@
+let val;
+
+function multipleByTwo(a,b,c){
+    let arr =[];
+    for(let i=0;i<3;i++){
+        arr[i]=arguments[i]*2 // fonksiyona gönderilen değerlere arguments ile ulaşılabilir
+    }
+    return arr; // doldurduğumuz diziyi geriye gönderiyoruz
+}
+
+val=multipleByTwo(5,10,20); // 2 katına adlık 
+
+//console.log(val);
+
+//------------------------------------------------
+
+function addOne(a){
+    return a+1;
+}
+
+//val=addOne(10); // 1kledik 
+
+//console.log(val1);
+
+//-----------------------------------
+
+    for(let i=0;i<val.length;i++){ // bu yaptığımız işlemi şimdi callback ile yapalım
+        val[i]=addOne(val[i]);
+    }
+
+console.log(val);  // 2 katına aldık sonra 1 ekledik
+
+//-------------------------------------------------------------------------
+
+let val1;
+
+function multipleByTwo1(a,b,c,callback){
+    let arr=[];
+    for(let i=0;i<3;i++){
+        arr[i]=callback(arguments[i]*2);
+    }
+    return arr;
+}
+
+function addOne(a) { 
+    return a+1;
+ }
+
+function addTwo(a){
+    return a+2; 
+}
+
+function addThree(a){
+    return a+3;
+}
+
+val1=multipleByTwo1(5,10,20,addThree); // ,addOne ,addTwo ,addThree gönderebiliriz.
+
+console.log(val1); // 2 ile çarptıktan sonra 3 eklemiş olduk
+
+//---------------------------------------------------------------------------------------------------
+
+let val3;
+
+function multipleByTwo2(a,b,c,callback){
+    let arr=[];
+
+    if(callback && typeof callback==="function"){ // dışarıdan gönderdiğimiz function değil ise boş bir dizi gelir hesaplama yapmaz
+        for(i=0;i<3;i++){
+            arr[i]=callback(arguments[i]*2);
+        }
+    }
+    return arr;
+}
+
+function addFour(a){
+    return a+4;
+}
+
+val3=multipleByTwo2(5,10,20,addFour);
+
+console.log(val3);
+
+//------------------------------------------------------------
+
+let val4;
+
+function multipleByTwo3(a,b,c,callback){
+    let arr=[];
+    for(let i=0;i<3;i++){
+        arr[i]=callback(arguments[i]*2);
+    }
+    return arr;
+}
+
+val4=multipleByTwo3(5,10,20,function (a){ // tek seferliğe mahsus kullanılacaksa anonymous function kullanılabilir. callback function olarak direkt burada kullanılabilir.
+    return a+5;
+});
+
+console.log(val4);
+
