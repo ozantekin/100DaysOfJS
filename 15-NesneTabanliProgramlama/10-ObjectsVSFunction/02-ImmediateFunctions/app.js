c38ab49e7c93fa826c8af2f51a8404c0eb9ba451
ozantekin <ardilozantekin@gmail.com>
2022-02-26 03:17:26 +0300

    ImmediateFunctions tamamlandı

---
+++ 15-NesneTabanliProgramlama/10-ObjectsVSFunction/02-ImmediateFunctions/app.js
@@ -0,0 +1,44 @@
+function welcome(){
+    var days=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+
+    var today = new Date ();
+
+    var msg="welcome. today is "+days[today.getDay()];
+
+    return msg; // mesajı geri gönderdik
+}
+
+console.log(welcome());
+
+// sayfa yüklendiği anda bu mesajın gelmesini istiyoruz bunun için immediate functionlar işimize yarayacak
+
+// 1.yol
+// (function(){
+
+// })(); 
+
+// 2.yol
+// (function (){
+
+// }()); 
+
+(function (){
+    var days=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+
+    var today = new Date ();
+
+    var msg="welcome. today is "+days[today.getDay()];
+
+    console.log(msg);
+}());
+//---------------------------------------------------------------------------------
+(function (name){
+    var days=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+
+    var today = new Date ();
+
+    var msg="welcome. " +name+ " today is "+days[today.getDay()];
+
+    console.log(msg);
+}("ozan"));
+
