38442c5ed9a4941518f17977a938c9cdce740e08
ozantekin <ardilozantekin@gmail.com>
2022-03-21 00:30:25 +0300

    yazım hatası düzeltildi

--- 21-RestfulApiProje/02-BilgiSorgulama/app.js
+++ 21-RestfulApiProje/02-BilgiSorgulama/app.js
@@ -13,7 +13,7 @@ searchProfile.addEventListener("keyup",(event)=>{
             if(res.profile.length===0){
                 // buraya boş tutmamızın nedeni her bir keyup olayı gerçekleştiğinde bir sorgualama yapılıyor sonucu yazılsın istemiyoruz 
             }else{
-                console.log(res.profile[0]) //sadece res ile dize (string) içinde bir profile bilgisi alırız. res.profile ile object gelir. [0] ile tüm object almak yerine 0.indexi alıyoruz bunu yapma amacımız bilgiyi direkt kullanmak istememiz
+                console.log(res.profile[0]) //sadece res ile dize (string) içinde bir profile bilgisi alırız. res.profile ile tüm object gelir. [0] ile tüm object almak yerine 0.indexi alıyoruz bunu yapma amacımız bilgiyi direkt kullanmak istememiz
             }
         });
    }
