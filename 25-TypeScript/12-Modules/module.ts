e80a805aa656188f251d9cce9208c3af7d922efe
Ozan Tekin <ardilozantekin@gmail.com>
2022-04-02 11:14:17 +0300

    hata düzeltildi

--- 25-TypeScript/12-Modules/module.ts
+++ 25-TypeScript/12-Modules/module.ts
@@ -1,9 +1,5 @@
 import {Taxi} from './taxi' ;
-import {Bus} from './bus';
   
   let taxi_1: Taxi = new Taxi({ x: 3, y: 5 });  // module.ts taxi ihitiyaç duyuyor
   taxi_1.travelTo({ x: 1, y: 2 }); 
-
-  let currentLocation = taxi_1.location; 
-  taxi_1.location = {x: 2, y: 5};
 
